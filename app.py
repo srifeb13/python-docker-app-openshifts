ccba5060383fd21a6e2dde4dc03404b0f8eed596
manee2k6 <manee2k6@gmail.com>
2019-08-07 19:15:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,10 +15,10 @@ def hello():
     except RedisError:
         visits = "<i>cannot connect to Redis, counter disabled</i>"
 
-    html = "<h3>Hello {name} Pattabhi team welcome to Docker Container..!</h3>" \
+    html = "<h3>Hello {name} TradeByte team welcome to Docker World..!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>" \
            "<b>Visits:</b> {visits}"
-    return html.format(name=os.getenv("NAME", "SoftGen"), hostname=socket.gethostname(), visits=visits)
+    return html.format(name=os.getenv("NAME", "Tradebyte"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
